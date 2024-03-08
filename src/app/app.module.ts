92f383d7f5f914d9a51852fe68360a2863c4d776
Ibrahim <ibrahimelothmani98@gmail.com>
2024-03-08 22:26:11 +0100

    Simple App using Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { AboutComponent } from './about/about.component';
+import { ListComponent } from './list/list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    AboutComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
